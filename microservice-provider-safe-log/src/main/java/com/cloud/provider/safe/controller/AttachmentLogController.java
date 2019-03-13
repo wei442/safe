8057aa91888f07d1251521e3fc2a0fb609a04db6
Administrator <Administrator@aaa-PC>
2019-03-13 21:52:31 +0800

    提交

--- microservice-provider-safe-log/src/main/java/com/cloud/provider/safe/controller/AttachmentLogController.java
+++ microservice-provider-safe-log/src/main/java/com/cloud/provider/safe/controller/AttachmentLogController.java
@@ -92,5 +92,4 @@ public class AttachmentLogController extends BaseController {
 		return attachmentLogResponse;
 	}
 
-
 }
--- microservice-provider-safe-log/src/main/java/com/cloud/provider/safe/controller/UserAppLoginLogController.java
+++ microservice-provider-safe-log/src/main/java/com/cloud/provider/safe/controller/UserAppLoginLogController.java
@@ -92,5 +92,4 @@ public class UserAppLoginLogController extends BaseController {
 		return userAppLoginLogResponse;
 	}
 
-
 }
